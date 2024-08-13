68a8349438b9c0e87eaaae9e15cab8b19ac9ebae
root <elliss.avd@gmail.com>
2024-08-13 12:42:25 +0300

    Initial Commit

---
+++ quiz app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz app
+//
+//  Created by Елизавета on 13.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
